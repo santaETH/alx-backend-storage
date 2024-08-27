20d4b5fe851d98bf5742139a72087c956c032eab
santaETH <sdamissie@yahoo.com>
2024-08-27 15:13:52 -0700

    NoSQL

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+9. Insert a document in Python
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''Inserts a new document in a collection.
+    '''
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
